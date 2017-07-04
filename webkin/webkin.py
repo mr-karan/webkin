e1faa07cd66d16036d17de0cc16f7d7b8e931853
Karan Sharma <karansharma1295@gmail.com>
2017-07-04 21:25:21 +0530

    handle img_url exception

--- webkin/webkin.py
+++ webkin/webkin.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-from .config  import MERCURY_API_KEY
-from .scaffold  import *
+from .config import MERCURY_API_KEY
+from .scaffold import *
 from .send_email import send_email
 
 from PIL import Image
@@ -29,8 +29,10 @@ args = parser.parse_args()
 if args.verbose:
     log.setLevel(DEBUG)
 
+
 def main():
     parse_url(url=args.url)
+
 
 def parse_url(url):
     directory = args.path or os.path.abspath(os.path.expanduser(os.curdir))
@@ -42,44 +44,53 @@ def parse_url(url):
 
     log.debug('Parsing {}'.format(url))
     parse_url = 'https://mercury.postlight.com/parser?url=' + url
-    data = requests.get(parse_url, headers={'x-api-key': MERCURY_API_KEY}).json()
+    data = requests.get(parse_url, headers={
+                        'x-api-key': MERCURY_API_KEY}).json()
     if data is None:
         log.info("Couldn't parse the webpage.")
         exit()
-    return convert_html(data,directory)
+    return convert_html(data, directory)
 
-def convert_html(data,directory):
+
+def convert_html(data, directory):
     command = ''
     try:
-        title = normalize('NFKD',data['title'])
+        title = normalize('NFKD', data['title'])
     except:
-        title='webpage'
-    filename = os.path.join(directory, title.replace(" ","_"))
-    html_file=filename+".html"
-    mobi_file=filename+".mobi"
-    img_file = filename+".png"
+        title = 'webpage'
+    filename = os.path.join(directory, title.replace(" ", "_"))
+    html_file = filename + ".html"
+    mobi_file = filename + ".mobi"
+    img_file = filename + ".png"
 
-    command +=' --title {} '.format(quote(title))
+    command += ' --title {} '.format(quote(title))
 
     if data['lead_image_url'] is not None:
-        img_url = data['lead_image_url']
-        im = Image.open(requests.get(img_url, stream=True).raw).resize((1200,1600),).save('{}'.format(img_file))
 
-        command += ' --cover {} '.format(img_file)
+        img_url = data['lead_image_url']
+        try:
+            im = Image.open(requests.get(img_url, stream=True).raw).resize(
+                (1200, 1600),).save('{}'.format(img_file))
+            command += ' --cover {} '.format(img_file)
+
+        except:
+            pass
 
     if data['excerpt'] is not None:
         excerpt = normalize('NFKD', data['excerpt'])
-        command += ' --comments {} --output-profile kindle'.format(quote(excerpt))
+        command += ' --comments {} --output-profile kindle'.format(
+            quote(excerpt))
 
     with open(os.path.join(directory, html_file), 'w') as template:
-        clean_html = normalize('NFKD',data['content'])
+        clean_html = normalize('NFKD', data['content'])
         template.write(clean_html)
 
     if _platform == "darwin":
         try:
             build_line = ' ./ebook-convert ' + html_file + " " + mobi_file + command
             args = split(build_line)
-            subprocess.call(args, cwd='/Applications/calibre.app/Contents/console.app/Contents/MacOS/')
+            subprocess.call(
+                args, cwd='/Applications/calibre.app/Contents/console.app/Contents/MacOS/')
         except:
             log.error('Please check if calibre CLI tools are installed.')
             exit()
@@ -95,7 +106,7 @@ def convert_html(data,directory):
     return send_email(mobi_file)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     log.info('Initiating Web-Kindle...')
     log.debug('Initiating Web-Kindle with DEBUG mode')
     if not check_for_tokens():
